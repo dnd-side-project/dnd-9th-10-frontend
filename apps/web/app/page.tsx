4b86c43b2712063b405db143f9bf395fce4ba8b7
bobeenlee <globaldev@naver.com>
2023-07-30 21:35:20 +0900

    fix: update import button

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect } from "react";
-import { Button } from "@dnd9-10/webui";
 import { getTest } from "@dnd9-10/shared";
 
 import styles from "../styles/index.module.css";
@@ -93,7 +92,6 @@ export default function Web() {
   return (
     <div className={styles.container}>
       <h1>Web</h1>
-      <Button onClick={() => console.log("Pressed!")} text="Boop" />
       <div>{getTest()}</div>
     </div>
   );
