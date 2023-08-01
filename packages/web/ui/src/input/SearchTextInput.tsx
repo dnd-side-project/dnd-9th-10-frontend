6ad7b7887b4f88c2e5c8c255f784529dc20c3946
이보빈[쇼핑라이브웹개발] <bobeen.lee@navercorp.com>
2023-08-01 20:08:05 +0900

    feat: update search input click

--- packages/web/ui/src/input/SearchTextInput.tsx
+++ packages/web/ui/src/input/SearchTextInput.tsx
@@ -2,17 +2,24 @@ import cn from "classnames";
 import styles from "./SearchTextInput.module.css";
 import TextInput, { TextInputProps } from "./TextInput";
 import Icon from "../icon/Icon";
+import IconButton from "../button/IconButton";
 
 export interface SearchTextInputProps extends TextInputProps {
   className?: string;
+  onSearch?: () => void;
 }
 
 export function SearchTextInput(props: SearchTextInputProps) {
-  const { className, ...restProps } = props;
+  const { className, onSearch, ...restProps } = props;
   return (
     <div className={cn(styles.wrap, className)}>
       <TextInput className={styles["input"]} {...restProps} />
-      <Icon className={styles["icon"]} name={"search"} size={20} />
+      <IconButton
+        className={styles["icon"]}
+        name={"search"}
+        size={20}
+        onClick={onSearch}
+      />
     </div>
   );
 }
