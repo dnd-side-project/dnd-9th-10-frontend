3709813c0488ecd2e76d7c64bbb5a9e56505a6fb
bobeenlee <globaldev@naver.com>
2023-08-05 13:23:33 +0900

    style: update checklist new page

--- apps/web/app/checklist/new/page.tsx
+++ apps/web/app/checklist/new/page.tsx
@@ -23,14 +23,17 @@ export default function Page() {
     <div className={styles.wrap}>
       <Topbar className={styles.topbar} onBackClick={handleBackOrHome} />
       <div className={styles.header}>
+        <Bold22 className={styles.title} as="p">
+          내 기준에서
+          <span className={styles.highlight}> 벗어난 친구 유형</span>
+          <br />
+          5가지를 선택해주세요.
+        </Bold22>
         <StepIndicator
           className={styles.indicator}
           current={1}
           totalCount={2}
         />
-        <Bold22 className={styles.title} as="pre">{`내 기준에서 벗어난
-친구 유형을 알려주세요`}</Bold22>
-        <Medium16 className={styles.subtitle}>{`최대 5개 선택 가능`}</Medium16>
       </div>
       <div className={styles.content}>
         <CheckList
@@ -40,9 +43,10 @@ export default function Page() {
             { name: "나를 깎아내리는 친구" },
           ]}
         />
+        <SubmitButton className={styles['add-button']} name="+ 기준 추가하기" />
       </div>
       <div className={styles.bottom}>
-        <SubmitButton className={styles.bottom} name="작성완료" />
+        <SubmitButton className={styles.bottom} name="선택 완료" />
       </div>
     </div>
   );
