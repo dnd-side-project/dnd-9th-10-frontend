e6c7374a2b994044b33740f50ea49272887eb5f8
JinseongHwang <jinseong.dev@gmail.com>
2023-08-06 16:32:37 +0900

    feat: create bookmark list and page

---
+++ packages/web/ui/src/bookmarks/BookmarkCardList.stories.tsx
@@ -0,0 +1,32 @@
+import { StoryFn, Meta } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+import { BookmarkCardList, BookmarkCardListProps } from './BookmarkCardList';
+
+export default {
+  component: BookmarkCardList,
+  title: 'bookmarks/BookmarkCardList',
+  argTypes: {},
+} as Meta;
+
+const Template: StoryFn<BookmarkCardListProps> = (args) => <BookmarkCardList {...args}
+data={[
+  {
+    description: "desc1",
+    reference: "ref",
+    active: true,
+  },
+  {
+    description: "desc2",
+    reference: "ref",
+    active: true,
+  },
+  {
+    description: "desc3",
+    reference: "ref",
+    active: true,
+  }
+]
+}/>;
+
+export const Primary = Template.bind({});
+Primary.args = {};
---
+++ packages/web/ui/src/bookmarks/BookmarkCardList.tsx
@@ -0,0 +1,20 @@
+import cn from 'classnames'
+import styles from './BookmarkCardList.module.css';
+import {BookmarkCard, BookmarkCardProps} from "./BookmarkCard";
+
+export interface BookmarkCardListProps {
+  className?: string
+  data: BookmarkCardProps[]
+}
+
+export function BookmarkCardList(props: BookmarkCardListProps) {
+  const { className , data} = props
+  return <div className={cn(styles.wrap, className)}>
+    {data?.map((item, index) => {
+      const {description, reference, active } = item
+      return <BookmarkCard key={index} description={description} reference={reference} active={active}/>
+    })}
+  </div>;
+}
+
+export default BookmarkCardList;
