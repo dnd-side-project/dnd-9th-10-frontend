5d672b4a81dd2ebce09c713ff500030417f322e4
bobeenlee <globaldev@naver.com>
2023-08-12 14:08:28 +0900

    feat: result 페이지 추가

--- apps/web/app/account/kakao/result/page.tsx
+++ apps/web/app/account/kakao/result/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 import { kakaoLogin } from "../../../../apis/account";
 
 type Props = {
@@ -7,20 +10,35 @@ type Props = {
   };
 };
 
-
-export default async function Page(props: Props) {
-  const { searchParams } = props
-  console.log(props)
-  const code = searchParams?.code
-
-  try {
-    const response = await kakaoLogin(code)
-    if(response.data?.memberId) {
-      redirect('/')
+export default function Page(props: Props) {
+  const { searchParams } = props;
+  const code = searchParams?.code;
+  const [errorMessage, setErrorMessage] = useState('')
+  useEffect(() => {
+    async function redirectTo() {
+      try {
+        const response = await kakaoLogin(code);
+        if (response.data?.memberId) {
+          redirect("/?success=true");
+        }
+        console.log(response)
+      } catch(error) {
+        console.error(error)
+        setErrorMessage(error.message)
+      }
     }
-    redirect('/account/signin?error_code=401')
-  } catch(error) {
-    console.error(error)
-    redirect('/account/signin?error_message=' + error.message)
-  }
+    redirectTo();
+  }, [code]);
+
+  return <div>error: {errorMessage}</div>
+  // try {
+  //   const response = await kakaoLogin(code)
+  //   if(response.data?.memberId) {
+  //     redirect('/')
+  //   }
+  //   redirect('/account/signin?error_code=401')
+  // } catch(error) {
+  //   console.error(error)
+  //   redirect('/account/signin?error_message=' + error.message)
+  // }
 }
