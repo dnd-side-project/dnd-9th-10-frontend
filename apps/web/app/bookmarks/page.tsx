67d05b6fa0c228e29863d901873f65bf3ccd0543
JinseongHwang <jinseong.dev@gmail.com>
2023-08-17 03:45:35 +0900

    feat: 북마크 조회 API 연동

--- apps/web/app/bookmarks/page.tsx
+++ apps/web/app/bookmarks/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import {useCallback, useEffect, useState} from "react";
 import { SubmitButton } from "@dnd9-10/webui/src/button/SubmitButton";
 import {
   Bold22,
@@ -13,32 +13,41 @@ import {
 import styles from "./page.module.css";
 import Topbar from "@dnd9-10/webui/src/topbar/Topbar";
 import BookmarkCardList from "@dnd9-10/webui/src/bookmarks/BookmarkCardList";
+import {getBookmarks} from "../../apis/bookmark";
+import {SayingInfoDto} from "@dnd9-10/shared/src/__generate__/api";
+
 
 export default function Page() {
   const router = useRouter();
+
+  const [bookmarks, setBookmarks] = useState<Array<SayingInfoDto>>([]);
 
   const handleBackClick = useCallback(() => {
     router.back();
   }, [router]);
 
+  useEffect(() => {
+    async function get() {
+      try {
+        const response = await getBookmarks();
+        setBookmarks(response);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    get()
+  }, []);
+
   return <div className={styles.wrap}>
     <Topbar title="북마크" onBackClick={handleBackClick}/>
-    <BookmarkCardList className={styles.content} data={[
-      {
-        description: "궁극적으로 결혼이든 우정이든 관계에서 유대감을 형성하는 것은 대화다.",
-        reference: "아일랜드 작가, 오스카 와일드",
-        active: true,
-      },
-      {
-        description: "모두가 너를 힘들게 할 거야 그게 진실이고. 넌 그럴 가치가 있는 놈 하나만 찾으면 돼.",
-        reference: "레게 선구자, 밥 말리",
-        active: true,
-      },
-      {
-        description: "고난과 불행이 찾아올 때 비로소 친구가 친구임을 안다.",
-        reference: "중국 당나라 시인, 이태백",
-        active: true,
-      }
-    ]}/>
+    <BookmarkCardList className={styles.content} data={
+      bookmarks.map(bookmark => {
+        return {
+          description: bookmark.contents,
+          reference: bookmark.reference,
+          active: true,
+        };
+      })
+    }/>
   </div>;
 }
