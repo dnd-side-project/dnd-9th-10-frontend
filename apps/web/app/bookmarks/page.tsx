23d2bc02bbbe1bcaf4ec11054eda2cf61554fb68
JinseongHwang <jinseong.dev@gmail.com>
2023-08-17 04:23:33 +0900

    feat: 조회 시 react query를 사용하도록 변경

--- apps/web/app/bookmarks/page.tsx
+++ apps/web/app/bookmarks/page.tsx
@@ -14,34 +14,22 @@ import styles from "./page.module.css";
 import Topbar from "@dnd9-10/webui/src/topbar/Topbar";
 import BookmarkCardList from "@dnd9-10/webui/src/bookmarks/BookmarkCardList";
 import {getBookmarks} from "../../apis/bookmark";
-import {SayingInfoDto} from "@dnd9-10/shared/src/__generate__/api";
+import {useQuery} from "@tanstack/react-query";
 
 
 export default function Page() {
   const router = useRouter();
 
-  const [bookmarks, setBookmarks] = useState<Array<SayingInfoDto>>([]);
-
   const handleBackClick = useCallback(() => {
     router.back();
   }, [router]);
 
-  useEffect(() => {
-    async function get() {
-      try {
-        const response = await getBookmarks();
-        setBookmarks(response);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-    get()
-  }, []);
+  const {data} = useQuery(["getBookmarks"], () => getBookmarks())
 
   return <div className={styles.wrap}>
     <Topbar title="북마크" onBackClick={handleBackClick}/>
     <BookmarkCardList className={styles.content} data={
-      bookmarks.map(bookmark => {
+      (data ?? []).map(bookmark => {
         return {
           description: bookmark.contents,
           reference: bookmark.reference,
