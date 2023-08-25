6f7b4b4f3ee168e644c2eb934ed87d00d70496fe
이보빈[쇼핑라이브웹개발] <bobeen.lee@navercorp.com>
2023-08-25 14:16:44 +0900

    fix: fixed error

--- apps/web/libs/kakao.ts
+++ apps/web/libs/kakao.ts
@@ -3,13 +3,13 @@ import { env } from "./env";
 export const initKakao = () => {
   const jsKey = "77b55322759c73e5f5a9dacf781cfa2c";
 
-  if (!window.Kakao.isInitialized()) {
-    window.Kakao.init(jsKey);
+  if (!window.Kakao?.isInitialized?.()) {
+    window.Kakao?.init?.(jsKey);
   }
 };
 
 export const loginWithKakao = () => {
-  window.Kakao.Auth.authorize({
+  window.Kakao?.Auth?.authorize?.({
     redirectUri: env.URL + "/account/kakao/result",
   });
 };
